4f92ab741072a6bc8aae449256519fecb165a7d5
twa04175 <twa04175@naver.com>
2022-05-08 13:22:05 +0900

    socket config, module add

--- src/app.ts
+++ src/app.ts
@@ -5,27 +5,35 @@ import express from 'express';
 import helmet from 'helmet'; //서버 보안용 미들웨어
 import hpp from 'hpp'; //매개변수 공격 보안용 미들웨어
 import morgan from 'morgan'; //http 로깅 미들웨어
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS, SOCKET_PORT } from '@config';
 import { Routes } from '@interfaces/routes.interface';
 import errorMiddleware from '@middlewares/error.middleware';
 import { logger, stream } from '@utils/logger';
+import { Server } from 'socket.io';
 
 class App {
   public app: express.Application;
   public env: string;
   public port: string | number;
 
+  public io: Server;
+  public socketPort: string | number;
+
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || 'development';
     this.port = PORT || 3000;
+    this.socketPort = SOCKET_PORT || 3001;
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
+    this.initializeSocket(this.socketPort);
   }
 
   public listen() {
+    this.io.on('connection', () => {});
+
     this.app.listen(this.port, () => {
       logger.info(`=================================`);
       logger.info(`======= ENV: ${this.env} =======`);
@@ -57,6 +65,30 @@ class App {
 
   private initializeErrorHandling() {
     this.app.use(errorMiddleware);
+  }
+
+  private initializeSocket(socketPort) {
+    interface ServerToClientEvents {
+      noArg: () => void;
+      basicEmit: (a: number, b: string, c: Buffer) => void;
+      withAck: (d: string, callback: (e: number) => void) => void;
+    }
+
+    interface ClientToServerEvents {
+      hello: () => void;
+    }
+
+    interface InterServerEvents {
+      ping: () => void;
+    }
+
+    interface SocketData {
+      name: string;
+      age: number;
+    }
+
+    console.log('app.ts:initializeSocket:71 -> open', socketPort);
+    this.io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(socketPort);
   }
 }
 
